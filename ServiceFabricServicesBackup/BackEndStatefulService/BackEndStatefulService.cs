c97bb89d53f8bbffea9fc123be4ed030a6e113b7
Kevin Harris (CANADA) <Kevin.Harris@microsoft.com>
2017-05-01 11:23:39 -0400

    Updated to newer version of ASF SDK.

---
+++ ServiceFabricServicesBackup/BackEndStatefulService/BackEndStatefulService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+using SharedLibrary;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+using Microsoft.ServiceFabric.Data;
+
+namespace BackEndStatefulService
+{
+    /// <summary>
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class BackEndStatefulService : StatefulService, ICameras
+    {
+        public BackEndStatefulService(StatefulServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <remarks>
+        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
+        /// </remarks>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            return new List<ServiceReplicaListener>()
+            {
+                new ServiceReplicaListener((context) => this.CreateServiceRemotingListener(context))
+            };
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service replica.
+        /// This method executes when this replica of your service becomes primary and has write status.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            IReliableDictionary<string, long> camerasDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("Cameras");
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                try
+                {
+                    using (ITransaction tx = this.StateManager.CreateTransaction())
+                    {
+                        ConditionalValue<long> result = await camerasDictionary.TryGetValueAsync(tx, "CameraCount");
+
+                        ServiceEventSource.Current.ServiceMessage(this, "Current Camera Count Value: {0}", result.HasValue ? result.Value.ToString() : "Value does not exist.");
+
+                        await camerasDictionary.AddOrUpdateAsync(tx, "CameraCount", 0, (key, value) => ++value);
+
+                        // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
+                        // discarded, and nothing is saved to the secondary replicas.
+                        await tx.CommitAsync();
+                    }
+
+                    await Task.Delay(TimeSpan.FromSeconds(60), cancellationToken);
+                }
+                catch (TimeoutException)
+                {
+                    //Service Fabric uses timeouts on collection operations to prevent deadlocks.
+                    //If this exception is thrown, it means that this transaction was waiting the default
+                    //amount of time (4 seconds) but was unable to acquire the lock. In this case we simply
+                    //retry after a random backoff interval. You can also control the timeout via a parameter
+                    //on the collection operation.
+                    Thread.Sleep(TimeSpan.FromSeconds(new Random().Next(100, 300)));
+
+                    continue;
+                }
+                catch (Exception exception)
+                {
+                    //For sample code only: simply trace the exception.
+                    ServiceEventSource.Current.ServiceMessage(this, exception.ToString());
+                }
+            }
+        }
+
+        public async Task<long> GetCamerasCountAsync()
+        {
+            IReliableDictionary<string, long> camerasDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("Cameras");
+
+            using (ITransaction tx = this.StateManager.CreateTransaction())
+            {
+                ConditionalValue<long> result = await camerasDictionary.TryGetValueAsync(tx, "CameraCount");
+                return result.HasValue ? result.Value : 0;
+            }
+        }
+
+        public async Task<long> AddCameraAsync()
+        {
+            IReliableDictionary<string, long> camerasDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("Cameras");
+            long newCameraCount = 0;
+
+            using (ITransaction tx = this.StateManager.CreateTransaction())
+            {
+                newCameraCount = await camerasDictionary.AddOrUpdateAsync(tx, "CameraCount", 0, (key, value) => ++value);
+
+                await tx.CommitAsync();
+            }
+
+            return newCameraCount;
+        }
+    }
+}
---
+++ ServiceFabricServicesBackup/FrontEndWebServer/Controllers/CamerasController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Fabric;
+using SharedLibrary;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+
+namespace FrontEndWebServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class CamerasController : Controller
+    {
+        private static readonly Uri backEndServiceUri;
+
+        static CamerasController()
+        {
+            backEndServiceUri = new Uri(FabricRuntime.GetActivationContext().ApplicationName + "/BackEndStatefulService");
+        }
+
+        // GET api/cameras
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            long camerasCount = 0;
+
+            try
+            {
+                Microsoft.ServiceFabric.Services.Client.ServicePartitionKey partKey = new Microsoft.ServiceFabric.Services.Client.ServicePartitionKey(0);
+                ICameras cameras = ServiceProxy.Create<ICameras>(backEndServiceUri, partKey, Microsoft.ServiceFabric.Services.Communication.Client.TargetReplicaSelector.Default, null);
+
+                camerasCount = await cameras.GetCamerasCountAsync();
+            }
+            catch (Exception exc)
+            {
+                System.Diagnostics.Trace.WriteLine(String.Format("Error: {0}", exc.Message));
+            }
+
+            System.Diagnostics.Trace.WriteLine(String.Format(" Camera Count: {0}", camerasCount));
+            return new string[] { camerasCount.ToString() };
+        }
+
+        // GET api/cameras/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/cameras
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            long camerasCount = 0;
+
+            try
+            {
+                Microsoft.ServiceFabric.Services.Client.ServicePartitionKey partKey = new Microsoft.ServiceFabric.Services.Client.ServicePartitionKey(0);
+                ICameras cameras = ServiceProxy.Create<ICameras>(backEndServiceUri, partKey, Microsoft.ServiceFabric.Services.Communication.Client.TargetReplicaSelector.Default, null);
+
+                camerasCount = await cameras.AddCameraAsync();
+            }
+            catch (Exception exc)
+            {
+                System.Diagnostics.Trace.WriteLine(String.Format("Error: {0}", exc.Message));
+            }
+
+            System.Diagnostics.Trace.WriteLine(String.Format(" Camera Count: {0}", camerasCount));
+
+            return this.Ok(String.Format("Count: {0}", camerasCount.ToString()));
+        }
+
+        // PUT api/cameras/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/cameras/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
